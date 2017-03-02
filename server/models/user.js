bd6f06d5783aee5e9d6d7d97d577f663f06ab0d4
PeterCondick <pcondick@Peters-MacBook-Pro.local>
2017-03-02 16:37:53 +1100

    added authentication

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,27 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    // return new Promise((resolve, reject) => {
+    //   reject();
+    // });
+    return Promise.reject();
+  }
+
+  // need the quotes when there is a . in the var name
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
