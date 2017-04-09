c7b8336ae580a7fc600fc158708fe610e0d57eec
PeterCondick <pcondick@Peters-MacBook-Pro.local>
2017-04-09 17:29:06 +1000

    added logout route

--- server/server.js
+++ server/server.js
@@ -145,7 +145,6 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
-// POST /users/login {email, password}
 app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
 
@@ -185,6 +184,14 @@ app.post('/users/login', (req, res) => {
 //   });
 //
 // });
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
